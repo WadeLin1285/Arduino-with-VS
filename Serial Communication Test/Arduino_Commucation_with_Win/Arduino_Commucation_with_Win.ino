f99e2d578d113536fb793e3491d79fa2497ba009
WadeLin1285 <wadeclin@gmail.com>
2018-06-13 18:57:42 +0800

    Add some comment in code file

--- Serial Communication Test/Arduino_Commucation_with_Win/Arduino_Commucation_with_Win.ino
+++ Serial Communication Test/Arduino_Commucation_with_Win/Arduino_Commucation_with_Win.ino
@@ -1,13 +1,15 @@
 #define BAUD 9600
+
 int num1,num2,num3 = 0;
+
 void setup(){
   Serial.begin(BAUD);
 }
 
 void loop(){
-  num1 = 0.15;
-  num2 = 0;
-  num3 = 0;
+  num1 = 0.15;         // type any number to test
+  num2 = 0.64;         // type any number to test
+  num3 = 0.87;         // type any number to test
   Serial.print(num1);
   Serial.print(" ");
   Serial.print(num2);
